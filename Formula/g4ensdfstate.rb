c5bc709b1f05ac1c6ca5a5cb6f7261db966399ba
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2022-10-20 14:35:43 +0100

    g4ensdfstate: use full website name to fix audit

--- Formula/g4ensdfstate.rb
+++ Formula/g4ensdfstate.rb
@@ -1,6 +1,6 @@
 class G4ensdfstate < Formula
   desc "Geant4 data library for nuclide properties"
-  homepage "https://geant4.cern.ch"
+  homepage "https://geant4.web.cern.ch"
   url "https://geant4-data.web.cern.ch/geant4-data/datasets/G4ENSDFSTATE.2.3.tar.gz"
   sha256 "9444c5e0820791abd3ccaace105b0e47790fadce286e11149834e79c4a8e9203"
   license ""
