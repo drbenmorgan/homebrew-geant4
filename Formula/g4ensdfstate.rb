cbee76a1674d58e3632b13c0dde4c5210977a423
Ben Morgan <1102642+drbenmorgan@users.noreply.github.com>
2022-10-20 13:51:36 +0000

    g4ensdfstate: add 2.3 bottle.

--- Formula/g4ensdfstate.rb
+++ Formula/g4ensdfstate.rb
@@ -5,6 +5,14 @@ class G4ensdfstate < Formula
   sha256 "9444c5e0820791abd3ccaace105b0e47790fadce286e11149834e79c4a8e9203"
   license ""
 
+  bottle do
+    root_url "https://ghcr.io/v2/drbenmorgan/geant4"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, monterey:     "4da9667d2f91ce11377bbe68ad652c7c6f37b9e9c49ecde2a6ef7d50b3573af3"
+    sha256 cellar: :any_skip_relocation, big_sur:      "ef779219b2e52c06e92ec3d6885c6e7bd44c5c44dd08d81dd6e9fa9dce061052"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "149254f26e243dffc99ca8d6cc31489682b987e93ff32eca69ebd380edc9211a"
+  end
+
   def install
     (pkgshare/buildpath.basename.to_s).install Dir["./*"]
   end
