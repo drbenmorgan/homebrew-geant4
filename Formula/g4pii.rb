45c598956265b20cd0b77a201da29eb558a8be82
Ben Morgan <1102642+drbenmorgan@users.noreply.github.com>
2022-10-24 15:17:49 +0000

    g4pii: add 1.3_1 bottle.

--- Formula/g4pii.rb
+++ Formula/g4pii.rb
@@ -6,6 +6,11 @@ class G4pii < Formula
   license ""
   revision 1
 
+  bottle do
+    root_url "https://ghcr.io/v2/drbenmorgan/geant4"
+    sha256 cellar: :any_skip_relocation, all: "7ccabdab26235f90aff5a18f9a1dded32cf1563766a7c64334ef1f3359b33e24"
+  end
+
   def install
     (pkgshare/buildpath.basename.to_s).install Dir["./*"]
   end
