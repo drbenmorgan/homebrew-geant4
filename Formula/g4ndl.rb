46d4222bf3d56c2490b7c8c3a7a8169134145be3
Ben Morgan <1102642+drbenmorgan@users.noreply.github.com>
2022-10-24 15:17:48 +0000

    g4ndl: add 4.6_1 bottle.

--- Formula/g4ndl.rb
+++ Formula/g4ndl.rb
@@ -6,6 +6,11 @@ class G4ndl < Formula
   license ""
   revision 1
 
+  bottle do
+    root_url "https://ghcr.io/v2/drbenmorgan/geant4"
+    sha256 cellar: :any_skip_relocation, all: "d538e5383851865e4ef81523f910d6d10e5cad900617ee8be6a6fb88ff794aaa"
+  end
+
   def install
     (pkgshare/buildpath.basename.to_s).install Dir["./*"]
   end
