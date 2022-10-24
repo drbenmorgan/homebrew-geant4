5fef717fde0675d43e648cbac7ccaf495f0b0bb0
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2022-10-24 13:37:18 +0100

    g4photonevaporation: revision bump

--- Formula/g4photonevaporation.rb
+++ Formula/g4photonevaporation.rb
@@ -4,6 +4,7 @@ class G4photonevaporation < Formula
   url "https://geant4-data.web.cern.ch/geant4-data/datasets/G4PhotonEvaporation.5.7.tar.gz"
   sha256 "761e42e56ffdde3d9839f9f9d8102607c6b4c0329151ee518206f4ee9e77e7e5"
   license ""
+  revision 1
 
   def install
     (pkgshare/buildpath.basename.to_s).install Dir["./*"]
