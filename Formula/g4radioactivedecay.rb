a6f9be488bbcb64e0168fb3ead70983c7aed0d45
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2022-10-24 13:37:54 +0100

    g4radioactivedecay: revision bump

--- Formula/g4radioactivedecay.rb
+++ Formula/g4radioactivedecay.rb
@@ -4,6 +4,7 @@ class G4radioactivedecay < Formula
   url "https://geant4-data.web.cern.ch/geant4-data/datasets/G4RadioactiveDecay.5.6.tar.gz"
   sha256 "3886077c9c8e5a98783e6718e1c32567899eeb2dbb33e402d4476bc2fe4f0df1"
   license ""
+  revision 1
 
   def install
     (pkgshare/buildpath.basename.to_s).install Dir["./*"]
