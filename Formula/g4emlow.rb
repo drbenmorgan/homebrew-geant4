755b5d05134d2d5151a3f9098eabf33bdb5baa6e
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2022-10-24 13:37:18 +0100

    g4emlow: revision bump

--- Formula/g4emlow.rb
+++ Formula/g4emlow.rb
@@ -4,6 +4,7 @@ class G4emlow < Formula
   url "https://geant4-data.web.cern.ch/geant4-data/datasets/G4EMLOW.8.0.tar.gz"
   sha256 "d919a8e5838688257b9248a613910eb2a7633059e030c8b50c0a2c2ad9fd2b3b"
   license ""
+  revision 1
 
   def install
     (pkgshare/buildpath.basename.to_s).install Dir["./*"]
