c917cb3b1ca41b660f89e9f50fd4f977db5907d7
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2022-10-24 13:37:18 +0100

    g4particlexs: revision bump

--- Formula/g4particlexs.rb
+++ Formula/g4particlexs.rb
@@ -4,6 +4,7 @@ class G4particlexs < Formula
   url "https://geant4-data.web.cern.ch/geant4-data/datasets/G4PARTICLEXS.4.0.tar.gz"
   sha256 "9381039703c3f2b0fd36ab4999362a2c8b4ff9080c322f90b4e319281133ca95"
   license ""
+  revision 1
 
   def install
     (pkgshare/buildpath.basename.to_s).install Dir["./*"]
