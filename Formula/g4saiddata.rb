336734a588ee7e908d94174c1b6502dbea3368cf
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2022-10-24 13:37:55 +0100

    g4saiddata: revision bump

--- Formula/g4saiddata.rb
+++ Formula/g4saiddata.rb
@@ -4,6 +4,7 @@ class G4saiddata < Formula
   url "https://geant4-data.web.cern.ch/geant4-data/datasets/G4SAIDDATA.2.0.tar.gz"
   sha256 "1d26a8e79baa71e44d5759b9f55a67e8b7ede31751316a9e9037d80090c72e91"
   license ""
+  revision 1
 
   def install
     (pkgshare/buildpath.basename.to_s).install Dir["./*"]
