34c030b552b46231fbf04c69f639c00c80ba8113
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2022-10-24 13:37:18 +0100

    g4ndl: revision bump

--- Formula/g4ndl.rb
+++ Formula/g4ndl.rb
@@ -4,6 +4,7 @@ class G4ndl < Formula
   url "https://geant4-data.web.cern.ch/geant4-data/datasets/G4NDL.4.6.tar.gz"
   sha256 "9d287cf2ae0fb887a2adce801ee74fb9be21b0d166dab49bcbee9408a5145408"
   license ""
+  revision 1
 
   def install
     (pkgshare/buildpath.basename.to_s).install Dir["./*"]
